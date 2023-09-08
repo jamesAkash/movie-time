b3f1576dd639d016a12e4d067974a49de3a2062a
jamesAkash <akash.james@outlook.com>
2023-09-08 20:45:22 +0530

    side re uasable star component

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -10,52 +11,84 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
+//prop Types
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+  // maxRating: PropTypes.number.isRequired,
 };
 
-const StarRating = ({ maxRating: starCount = 2 }) => {
-  const [rating, setRating] = useState(0);
+const StarRating = ({
+  maxRating: starCount = 6,
+  color = "#fcc419",
+  size = "48",
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) => {
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0);
   function handleRating(rating) {
     setRating(rating);
+    onSetRating && onSetRating(rating);
   }
 
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    fontSize: `${size / 1.5}px`,
+    color,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: starCount }, (_, i) => (
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
-            onGlide={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={rating >= i + 1 || tempRating >= i + 1}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === starCount
+          ? messages[rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 };
 
-const Star = ({ onRate, onGlide, full }) => {
+const Star = ({ onRate, onHoverIn, onHoverOut, full, color, size }) => {
   return (
     <span
       style={{
-        width: "48px",
-        height: "48px",
+        width: `${size}px`,
+        height: `${size}px`,
         display: "block",
         cursor: "pointer",
       }}
       role="button"
       onClick={onRate}
-      onMouseEnter={onGlide}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
     >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
+          fill={color}
           stroke="#000"
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -65,7 +98,7 @@ const Star = ({ onRate, onGlide, full }) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
